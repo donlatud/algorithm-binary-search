291d20f70d28bbe51ebb288623f42456d94ea9ec
donlatud <donlatud@gmail.com>
2025-12-25 09:51:39 +0700

    feat: implement binary search functions for number and student lookups

--- th/ex-2.js
+++ th/ex-2.js
@@ -13,6 +13,23 @@
 */
 
 // Start coding here
+const updateStudentScore = (studentsInfo, targetStudent, addScore) => {
+  let left = 0;
+  let right = studentsInfo.length - 1;
+  
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (studentsInfo[mid].name === targetStudent) {
+      studentsInfo[mid].score += addScore;
+      return `${targetStudent} score is now ${studentsInfo[mid].score}`;
+    } else if (studentsInfo[mid].name < targetStudent) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return `Not found ${targetStudent} on the list.`;
+};
 
 const studentsInfo = [
   { name: "Alice", age: 14, classNumber: 8, score: 70 },
